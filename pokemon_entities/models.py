4be4cb9e246d554d23837cb057c01144324309ce
Максим Калинин <maksim.kalinin1@gmail.com>
2022-10-13 12:04:48 +0300

    Initial commit

---
+++ pokemon_entities/models.py
@@ -0,0 +1,43 @@
+from django.db import models  # noqa F401
+import django
+
+
+class Pokemon(models.Model):
+    title = models.CharField(verbose_name='имя покемона', max_length=200)
+    title_en = models.CharField(verbose_name='имя покемона на английском', max_length=200, blank=True)
+    title_jp = models.CharField(verbose_name='имя покемона на японском', max_length=200, blank=True)
+    photo = models.ImageField(verbose_name='изображение покемона', null=True)
+    description = models.TextField(verbose_name='описание покемона', blank=True)
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        verbose_name='из кого эволюционирует',
+        related_name='children',
+        null=True,
+        blank=True,
+     )
+    
+    def __str__(self):
+        return f'{self.title}'
+
+class PokemonEntity(models.Model):
+    lat = models.FloatField(verbose_name='широта', null=True, blank=True)
+    lon = models.FloatField(verbose_name='долгота', null=True, blank=True)
+    pokemon = models.ForeignKey(Pokemon, verbose_name='покемон', on_delete=models.CASCADE)
+    appeared_at = models.DateTimeField(
+        verbose_name='когда появился',
+        null=True,
+        blank=True,        
+        default=django.utils.timezone.now(),
+     )
+    disappeared_at = models.DateTimeField(
+        verbose_name='когда пропал',
+        null=True,
+        blank=True,        
+        default=django.utils.timezone.now(),
+     )
+    level = models.IntegerField(verbose_name='уровень', default=1)
+    health = models.IntegerField(verbose_name='здоровье', default=1)
+    strength = models.IntegerField(verbose_name='сила', default=1)
+    defence = models.IntegerField(verbose_name='защита',default=1)
+    stamina = models.IntegerField(verbose_name='выносливость', default=1)
---
+++ pokemon_entities/views.py
@@ -0,0 +1,114 @@
+import folium
+import json
+import django
+
+from django.http import HttpResponseNotFound
+from django.shortcuts import render
+from pokemon_entities.models import Pokemon, PokemonEntity
+
+
+MOSCOW_CENTER = [55.751244, 37.618423]
+DEFAULT_IMAGE_URL = (
+    'https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision'
+    '/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832'
+    '&fill=transparent'
+)
+
+
+def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
+    icon = folium.features.CustomIcon(
+        image_url,
+        icon_size=(50, 50),
+    )
+    folium.Marker(
+        [lat, lon],
+        # Warning! `tooltip` attribute is disabled intentionally
+        # to fix strange folium cyrillic encoding bug
+        icon=icon,
+    ).add_to(folium_map)
+
+
+def show_all_pokemons(request):
+    #with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
+        #pokemons = json.load(database)['pokemons']
+    pokemons = Pokemon.objects.all()
+    pokemon_entity = PokemonEntity.objects.filter(
+        appeared_at__lte=django.utils.timezone.localtime(),
+        disappeared_at__gte=django.utils.timezone.localtime(),
+     )
+
+    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
+    for pokemon in pokemon_entity:
+        add_pokemon(
+                folium_map, pokemon.lat,
+                pokemon.lon,
+                request.build_absolute_uri(pokemon.pokemon.photo.url)
+            )
+    pokemons_on_page = []
+    for pokemon in pokemons:
+        try:
+            url = pokemon.photo.url
+            pokemons_on_page.append({
+                'pokemon_id': pokemon.id,
+                'img_url': request.build_absolute_uri(url),
+                'title_ru': pokemon.title,
+            })
+        except ValueError:
+            pokemons_on_page.append({
+            'pokemon_id': pokemon.id,
+            'title_ru': pokemon.title,
+            })
+
+    return render(request, 'mainpage.html', context={
+        'map': folium_map._repr_html_(),
+        'pokemons': pokemons_on_page,
+    })
+
+
+def show_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=int(pokemon_id))
+    pokemon_entity = PokemonEntity.objects.filter(
+        pokemon__id=int(pokemon_id),
+        appeared_at__lte=django.utils.timezone.localtime(),
+        disappeared_at__gte=django.utils.timezone.localtime(),
+     )
+    next_evolution = {}
+    previous_evolution = {}
+    if pokemon.children.all():
+        pokemon_children = pokemon.children.all()[0]
+        next_evolution = {
+            'title_ru': pokemon_children.title,
+            'pokemon_id': pokemon_children.id,
+            'img_url': request.build_absolute_uri(pokemon_children.photo.url),
+        }
+    else:
+        pokemon_children = None
+    if pokemon.parent:
+        previous_evolution = {
+            'title_ru': pokemon.parent.title,
+            'pokemon_id': pokemon.parent.id,
+            'img_url': request.build_absolute_uri(pokemon.parent.photo.url),
+        }
+    else:
+        previous_evolution = None
+    requested_pokemon = {
+                'pokemon_id': pokemon.id,
+                'img_url': request.build_absolute_uri(pokemon.photo.url),
+                'title_ru': pokemon.title,
+                'description': pokemon.description,
+                'title_en': pokemon.title_en,
+                'title_jp': pokemon.title_jp,
+                'previous_evolution': previous_evolution,
+                'next_evolution': next_evolution,
+             }
+    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
+    for pokemon in pokemon_entity:
+        add_pokemon(
+            folium_map, pokemon.lat,
+            pokemon.lon,
+            request.build_absolute_uri(pokemon.pokemon.photo.url)
+        )
+
+    return render(request, 'pokemon.html', context={
+        'map': folium_map._repr_html_(), 'pokemon': requested_pokemon
+    })
